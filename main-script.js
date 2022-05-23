11f8d8a5a9309934b6997ffb4304882cf496b68c
jeffbalagosa <61676139+jeffbalagosa@users.noreply.github.com>
2022-05-22 19:31:15 -0700

    Add functionality to enable & disable remove picked items checkbox.

--- main-script.js
+++ main-script.js
@@ -1,10 +1,9 @@
-// ON input event of picker mode, enable the Remove Items
-
 // define the target radio button for the event listener
 let pickerMode = document.getElementById("flexRadioDefault1");
 let shuffleMode = document.getElementById("flexRadioDefault2");
 let removeItemSelect = document.getElementById("flexCheckDefault");
 
+// define function to enable checkbox if picker mode is checked
 function enableCheckbox() {
   if (pickerMode.checked) {
     removeItemSelect.disabled = false;
@@ -14,6 +13,7 @@ function enableCheckbox() {
   }
 }
 
+// define function to enable checkbox if suffle mode is checked
 function disableCheckbox() {
   if (shuffleMode.checked) {
     removeItemSelect.disabled = true;
@@ -23,5 +23,6 @@ function disableCheckbox() {
   }
 }
 
+// Use event listeners on radio buttons to run functions
 pickerMode.addEventListener("change", enableCheckbox);
 shuffleMode.addEventListener("change", disableCheckbox);
