12c04a86e6d2a10611e552419192957625ffe767
jeffbalagosa <61676139+jeffbalagosa@users.noreply.github.com>
2022-06-13 20:00:42 -0700

    Remove tables on paste of textbox.

--- main-script.js
+++ main-script.js
@@ -45,6 +45,18 @@ function addHTMLTags(arr, tagType) {
   return newArr;
 }
 
+// Turn anything pasted into the textbox into plain text
+textBox.onpaste = function (e) {
+  // cancel paste
+  e.preventDefault();
+
+  // get text representation of clipboard
+  let text = (e.originalEvent || e).clipboardData.getData("text/plain");
+
+  // insert text manually
+  document.execCommand("insertText", false, text);
+};
+
 // Use event listeners on radio buttons
 pickerMode.addEventListener("change", enableCheckbox);
 shuffleMode.addEventListener("change", enableCheckbox);
